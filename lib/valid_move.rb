c8b44468e04d718944c5f0e045aeb08e6264a0a9
Russel Pugh <russ_scooby@yahoo.com>
2020-02-03 19:34:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,8 @@
 board = Array.new(9, " " || "")
 puts "Player "X", where would you like to go?"
 
-
+def valid_move?(board, index)
+  if 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
