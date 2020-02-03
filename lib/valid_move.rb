3fae238889f0461dc3dbcafd19c5990db6e253bd
Russel Pugh <russ_scooby@yahoo.com>
2020-02-03 19:41:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,8 @@ puts "Player "X", where would you like to go?"
 
 def valid_move?(board, index)
   if index.between(0, 8)
+    true
+  else
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
